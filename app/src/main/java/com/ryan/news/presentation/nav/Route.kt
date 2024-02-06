cbacb2650223200779f036eebfc97c2f3276f341
Kuro <kyawcthumoeoo@gmail.com>
2024-02-07 02:26:36 +0700

    add color, theme and route scaffold

--- app/src/main/java/com/ryan/news/presentation/nav/Route.kt
+++ app/src/main/java/com/ryan/news/presentation/nav/Route.kt
@@ -2,10 +2,30 @@ package com.ryan.news.presentation.nav
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.ryan.news.presentation.view.home.HomeView
+import com.ryan.news.presentation.view.search.SearchView
 
 @ExperimentalMaterial3Api
 @Composable
 internal fun Route() {
     val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.Home.route) {
+        composable(Routes.Home.route) {
+            HomeView(navHostController = navController)
+        }
+        composable(Routes.Search.route) {
+            SearchView(navHostController = navController)
+        }
+    }
+}
+
+sealed class Routes(val route : String) {
+    object Home : Routes("home")
+
+    object Search : Routes("search")
+
 }
--- app/src/main/java/com/ryan/news/presentation/view/ui/theme/Theme.kt
+++ app/src/main/java/com/ryan/news/presentation/view/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = AccentColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = AccentColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BaseandroidTheme(
+fun NewsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
